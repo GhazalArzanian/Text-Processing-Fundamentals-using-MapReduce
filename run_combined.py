1d5cbb5539bad433d1ba1a14af468b8211464243
annalaczko <annalaczko.maps@gmail.com>
2024-04-25 18:47:51 +0200

    Uploading scripts for easy access

---
+++ run_combined.py
@@ -0,0 +1,5 @@
+import os
+
+os.system("python categories.py --hadoop-streaming-jar /usr/lib/hadoop/tools/lib/hadoop-streaming-3.3.5.jar -r hadoop hdfs:///user/dic24_shared/amazon-reviews/full/reviewscombined.json > categories.txt")
+os.system("python term_distribution_final.py --hadoop-streaming-jar /usr/lib/hadoop/tools/lib/hadoop-streaming-3.3.5.jar --file stopwords.txt --file categories.txt -r hadoop hdfs:///user/dic24_shared/amazon-reviews/full/reviewscombined.json > output1.txt")
+os.system("python append_terms.py output1.txt > output.txt")
